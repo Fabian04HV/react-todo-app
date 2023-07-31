157079ef660d734cfe3ab180f31071cfe76aa268
c0ding2go <fabian04berlin@outlook.de>
2023-07-31 12:35:59 +0200

    IMPROVE: UX for Overlay and Modal AND: improve color palette

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -8,17 +8,15 @@ export const Home = () => {
   const { toggleDarkmodeHandler } = useContext(ThemeContext)
   const { user, logoutUser } = useContext(AuthContext)
   const authToken = localStorage.getItem('authToken')
-
-  const [showOverlay, setShowOverlay] = useState(false)
-  const [showCreateModal, setShowCreateModal] = useState(false)
-  const [showEditModal, setShowEditModal] = useState(false)
-
+  const [fetchCompleted, setFetchCompleted] = useState(false)
+  
   const [todos, setTodos] = useState([])
   const [newTask, setNewTask] = useState('')
-  const [newEditTask, setNewEditTask] = useState('')
-  const [editTask, setEditTask] = useState('')
 
-  const [fetchCompleted, setFetchCompleted] = useState(false)
+  const [newEditTask, setNewEditTask] = useState('')
+  const [selectedTaskToEdit, setSelectedTaskToEdit] = useState('')
+
+  const [showModal, setShowModal] = useState(false)
 
   useEffect(() => {
     fetchUsersTodos(user._id, authToken)
@@ -33,7 +31,7 @@ export const Home = () => {
   }, [todos])
 
   const handleNewTaskInput = (e) => setNewTask(e.target.value)  
-  const handleKeyDown = (e) => e.key == "Enter" && createTodo()
+  const handleKeyDown = (e) => e.key === "Enter" && createTodo()
 
   const createTodo = () => {
     //prevent duplicates
@@ -70,26 +68,26 @@ export const Home = () => {
   const handleNewEditTaskInput = (e) => setNewEditTask(e.target.value)
 
   const editTodo = (task) => {
-    setShowOverlay(true)
-    setShowEditModal(true)
+    setShowModal("edit")
     setNewEditTask(task)
-    setEditTask(task)
+    setSelectedTaskToEdit(task)
   }
 
   const confirmChange = () => {
     const updatedTodos = todos.map(todo => {
-      if(todo.task === editTask){
+      if(todo.task === selectedTaskToEdit){
         return { ...todo, task: newEditTask}
       }
       else return todo
     })
     setTodos(updatedTodos)
+    setShowModal(false)
   }
 
-  const closeModals = () => {
-    setShowEditModal(false)
-    setShowCreateModal(false)
-    setShowOverlay(false)
+  const handleCloseOverlay = (e) => {
+    if(e.target.className === "overlay"){
+      setShowModal(false)
+    }
   }
 
   if(!user){
@@ -98,11 +96,12 @@ export const Home = () => {
   else return(
     <div className="Home">
       <nav>
-        <button className="icon-center-button white-hover" title="Navigation">
+        {/* <button className="icon-center-button white-hover" title="Navigation">
           <svg fill="#fff" xmlns="http://www.w3.org/2000/svg" height="28" viewBox="0 -960 960 960" width="28"><path d="M150-240q-12.75 0-21.375-8.675-8.625-8.676-8.625-21.5 0-12.825 8.625-21.325T150-300h660q12.75 0 21.375 8.675 8.625 8.676 8.625 21.5 0 12.825-8.625 21.325T810-240H150Zm0-210q-12.75 0-21.375-8.675-8.625-8.676-8.625-21.5 0-12.825 8.625-21.325T150-510h660q12.75 0 21.375 8.675 8.625 8.676 8.625 21.5 0 12.825-8.625 21.325T810-450H150Zm0-210q-12.75 0-21.375-8.675-8.625-8.676-8.625-21.5 0-12.825 8.625-21.325T150-720h660q12.75 0 21.375 8.675 8.625 8.676 8.625 21.5 0 12.825-8.625 21.325T810-660H150Z"/></svg>
-        </button>
-        <button onClick={() => toggleDarkmodeHandler()} className="icon-center-button white-hover" title="Switch Theme">
+        </button> */}
+        <button onClick={() => toggleDarkmodeHandler()} className="white-hover" title="Switch Theme">
           <svg fill="#fff" xmlns="http://www.w3.org/2000/svg" height="28" viewBox="0 -960 960 960" width="28"><path d="M524-40q-84 0-157.5-32t-128-86.5Q184-213 152-286.5T120-444q0-146 93-257.5T450-840q-18 98 11 192.635 29 94.635 100 165.736 71 71.101 165.5 100.143Q821-352.445 920-370.471q-26 144.206-138 237.338Q670-40 524-40Zm0-60q100 0 182-57t132-145q-90-8-173-41.5T518.5-440Q455-503 422-585.5T381-757q-88 48-144.5 130.5T180-444q0 143.333 100.333 243.667Q380.667-100 524-100Zm-6-340Z"/></svg>
+          Theme
         </button>
         <span>{user.firstname} {user.lastname}</span>
         <button title="Logout" className="white-hover" onClick={() => logoutUser()}>
@@ -110,7 +109,6 @@ export const Home = () => {
           Logout
         </button>
       </nav>
-
       <main>
         <h1>{user.firstname}'s Todos</h1>
         <ul className="todo-list">
@@ -123,55 +121,55 @@ export const Home = () => {
                 checked={todo.isCompleted}
                 onChange={() => toggleTodoCompleted(todo.task)} 
                 />
-              <label htmlFor={`todo-${index}`}>{todo.task}</label>
-              <button onClick={() => editTodo(todo.task)} title="Edit" className="icon-center-button">
-                <svg fill="var(--text-color)" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M180-180h44l443-443-44-44-443 443v44Zm614-486L666-794l42-42q17-17 42-17t42 17l44 44q17 17 17 42t-17 42l-42 42Zm-42 42L248-120H120v-128l504-504 128 128Zm-107-21-22-22 44 44-22-22Z"/></svg>
-              </button>
+              <label className="todo" htmlFor={`todo-${index}`}>{todo.task}</label>
+              <label htmlFor="edit-modal-input" onClick={() => editTodo(todo.task)} title="Edit" className="icon-center-button">
+                <svg fill="var(--secondary-text-color)" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M180-180h44l443-443-44-44-443 443v44Zm614-486L666-794l42-42q17-17 42-17t42 17l44 44q17 17 17 42t-17 42l-42 42Zm-42 42L248-120H120v-128l504-504 128 128Zm-107-21-22-22 44 44-22-22Z"/></svg>
+              </label>
               <button onClick={() => deleteTodo(todo.task)} className="icon-center-button">
-                <svg fill="var(--text-color)" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="m249-207-42-42 231-231-231-231 42-42 231 231 231-231 42 42-231 231 231 231-42 42-231-231-231 231Z"/></svg>
+                <svg fill="var(--secondary-text-color)" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="m249-207-42-42 231-231-231-231 42-42 231 231 231-231 42 42-231 231 231 231-42 42-231-231-231 231Z"/></svg>
               </button>
               </li>
             )})
           }
         </ul>
         <footer>
-          <button title="Create New Todo" onClick={() => { setShowOverlay(true); setShowCreateModal(true)}} className="main-button add-todo-button">Add Todo</button>
+          <label htmlFor="modal-input" title="Create New Todo" onClick={() => { setShowModal("create")}} className="main-button add-todo-button">Add Todo</label>
         </footer>
-        {showOverlay && 
-        <div className={`overlay`}>
-          ${showCreateModal && 
+        {showModal && 
+        <div className={`overlay`} onClick={(e) => handleCloseOverlay(e)}>
+          ${showModal === "create" && 
           <div className={`create-todo-modal`}>
             <h2>Create New Todo</h2>
             <input 
+              id="modal-input"
               value={newTask}
               onKeyDown={handleKeyDown} 
               onChange={handleNewTaskInput} 
               type="text" 
-              placeholder="Enter your task and press ENTER" 
+              placeholder="Enter your task and press [ENTER]" 
               maxLength={80} />
-            <button title="close" onClick={() => closeModals()} className="icon-center-button close-x-button">
+            <button title="close" onClick={() => setShowModal(false)} className="icon-center-button close-x-button">
               <svg fill="var(--text-color)" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="m249-207-42-42 231-231-231-231 42-42 231 231 231-231 42 42-231 231 231 231-42 42-231-231-231 231Z"/></svg>
             </button>
           </div>}
-          ${showEditModal && 
+          ${showModal === "edit" && 
           <div className={`create-todo-modal`}>
             <h2>Edit Todo</h2>
             <input 
+              id="edit-modal-input"
               value={newEditTask}
               onKeyDown={handleEditKeyDown} 
               onChange={handleNewEditTaskInput} 
               type="text" 
               placeholder="Enter your task and press ENTER" 
               maxLength={80} />
-            <button title="close" onClick={() => closeModals()} className="icon-center-button close-x-button">
+            <button title="close" onClick={() => setShowModal(false)} className="icon-center-button close-x-button">
               <svg fill="var(--text-color)" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="m249-207-42-42 231-231-231-231 42-42 231 231 231-231 42 42-231 231 231 231-42 42-231-231-231 231Z"/></svg>
             </button>
             <button className="main-button" title="Confirm Change" onClick={() => confirmChange()}>Confirm Change</button>
           </div>}
-
         </div>
         }
-        
       </main>
     </div>
   )
