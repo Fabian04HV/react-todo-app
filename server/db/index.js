93c692e9cc07f8e3dc42c1202a550a21b7a1068b
c0ding2go <fabian04berlin@outlook.de>
2023-08-01 14:18:38 +0200

    setup project for deployment

--- server/db/index.js
+++ server/db/index.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 require('dotenv').config()
 
-const MONGO_URI = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/react-todo-app"
+const MONGO_URI = process.env.MONGO_URI //|| "mongodb://127.0.0.1:27017/react-todo-app"
 
 mongoose.connect(MONGO_URI, {
   useNewUrlParser: true,
